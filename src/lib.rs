52ecbd20277dcee7210d8e5e7a6fe259f909e7b6
Byron Batteson <byronbatteson@gmail.com>
2022-09-21 11:39:17 +0200

    Add TypeScript types
    This saves from having to create an intermediate NPM Tangibl package.

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,22 @@ impl From<&JsonTopCode> for TopCode {
     }
 }
 
-#[wasm_bindgen]
+/// There's no great support for TypeScript types at the moment, so this definition must be kept in
+/// sync with the Rust function(s) below.
+#[wasm_bindgen(typescript_custom_section)]
+const TS_APPEND_CONTENT: &'static str = r#"
+export interface TopCode {
+    code: number;
+    unit: number;
+    orientation: number;
+    x: number;
+    y: number;
+}
+
+export function parse(topcodes: TopCode[]): string;
+"#;
+
+#[wasm_bindgen(skip_typescript)]
 pub fn parse(topcodes: JsValue) -> String {
     if let Ok(codes) = serde_wasm_bindgen::from_value::<Vec<JsonTopCode>>(topcodes) {
         let mut json_printer = tangibl::JsonPrinter::new();
--- www/index.js
+++ www/index.js
@@ -1,16 +1,12 @@
-import * as wasm from "tangibl-wasm";
-
-export class TopCode {
-  constructor(code, unit, orientation, x, y) {
-    this.code = code;
-    this.unit = unit;
-    this.orientation = orientation;
-    this.x = x;
-    this.y = y;
-  }
-}
+import { parse } from "tangibl-wasm";
 
 // Simple start token -- consider swapping this out with a Tangibl builder.
-const topcodes = [new TopCode(61, 8, 0.0, 0.0, 0.0)];
+const topcodes = [{
+  code: 61,
+  unit: 8,
+  orientation: 0.0,
+  x: 0.0,
+  y: 0.0}
+];
 
-alert(wasm.parse(topcodes));
+alert(parse(topcodes));
