41f0fe37ae323bfe4dc5bf85f3a797bb9ca407a5
Byron Batteson <byronbatteson@gmail.com>
2023-06-11 09:53:45 +0200

    Add types and update parse return type
    The parse method now returns JavaScript objects instead of JSON strings.
    With this, are some TypeScript definitions of the output. These types
    are not guaranteed to be correct, so an additional integration test
    should be performed to parse a maximally representative Tangibl AST and
    ensure that the types correlate with the output.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use js_sys::JSON;
 use serde::{Deserialize, Serialize};
 use tangibl;
 use topcodes::TopCode;
@@ -44,6 +45,9 @@ impl From<&JsonTopCode> for TopCode {
 /// sync with the Rust function(s) below.
 #[wasm_bindgen(typescript_custom_section)]
 const TS_APPEND_CONTENT: &'static str = r#"
+/**
+ * The minimal interface required for Tangibl to parse a TopCode.
+ */
 export interface TopCode {
     code: number;
     unit: number;
@@ -52,16 +56,154 @@ export interface TopCode {
     y: number;
 }
 
-export function parse(topcodes: TopCode[]): string;
+/**
+ * Parse a set of TopCodes into a Tangibl AST
+ */
+export function parse(topcodes: TopCode[]): Start | undefined;
+
+/**
+ * This represents the group of all flow tokens. Most tokens are flow tokens.
+ */
+export type Flow = Command | BooleanMethod | IntegerMethod | Conditional;
+
+/**
+ * Represents the common properties of a flow token.
+ */
+export interface FlowShape {
+    next?: Flow;
+}
+
+/**
+ * The first token in a Tangibl program. Signifies the start of the control
+ * flow.
+ */
+export interface Start extends FlowShape {
+    name: "start";
+}
+
+/**
+ * Implementation-specific meaning, but generally represents a projectile of
+ * sorts being launched from a player's position.
+ */
+export interface Shoot extends FlowShape {
+    name: "shoot";
+}
+
+/**
+ * Move the player one tile forwards.
+ */
+export interface MoveForwards extends FlowShape {
+    name: "moveForwards";
+}
+
+/**
+ * Move the player one tile backwards.
+ */
+export interface MoveBackwards extends FlowShape {
+    name: "moveBackwards";
+}
+
+/**
+ * Turn the player left by rotating anti-clockwise by 90 degrees. Do not move
+ * the player.
+ */
+export interface TurnLeft extends FlowShape {
+    name: "turnLeft";
+}
+
+/**
+ * Turn the player left by rotating clockwise by 90 degrees. Do not move
+ * the player.
+ */
+export interface TurnRight extends FlowShape {
+    name: "turnRight";
+}
+
+/**
+ * A simple command type. Can be thought of as a statement.
+ */
+export type Command = Shoot | MoveForwards | MoveBackwards | TurnLeft | TurnRight;
+
+/**
+ * Inputs into boolean methods.
+ */
+export type Condition = "isBlocked" | "isClear";
+
+/**
+ * Represents the common properties of a boolean method token.
+ */
+export interface BooleanMethodShape extends FlowShape {
+    body?: Flow;
+    condition?: Condition;
+}
+
+/**
+ * A boolean method which represents a while loop.
+ */
+export interface While extends BooleanMethodShape {
+    name: "while";
+}
+
+/**
+ * A type which has an additional control flow based on the input predicate.
+ */
+export type BooleanMethod = While;
+
+/**
+ * A number of integers, as well as an "infinity" representation.
+ */
+export type Value = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "infinity";
+
+/**
+ * Represents the common properties of an integer method token.
+ */
+export interface IntegerMethodShape extends FlowShape {
+    body?: Flow;
+    value?: Value;
+}
+
+/**
+ * An integer method which represents a for/repeat loop.
+ */
+export interface Repeat extends IntegerMethodShape {
+    name: "repeat";
+}
+
+/**
+ * A type which has an additional control flow based on the input integer.
+ */
+export type IntegerMethod = Repeat;
+
+/**
+ * Represents the common properties of an conditional token.
+ */
+export interface ConditionalShape extends FlowShape {
+    alternate?: Flow;
+}
+
+/**
+ * A conditional where the predicate is based on whether the player has an
+ * obstacle in front of them.
+ */
+export interface Blocked extends ConditionalShape {
+    name: "blocked";
+}
+
+/**
+ * A type which has two control flow paths based on the result of some
+ * pre-defined predicate.
+ */
+export type Conditional = Blocked;
 "#;
 
 #[wasm_bindgen(skip_typescript)]
-pub fn parse(topcodes: JsValue) -> String {
+pub fn parse(topcodes: JsValue) -> JsValue {
     if let Ok(codes) = serde_wasm_bindgen::from_value::<Vec<JsonTopCode>>(topcodes) {
         let mut json_printer = tangibl::JsonPrinter::new();
         if let Some(start) = tangibl::parse(&codes.iter().map(|t| t.into()).collect()) {
-            return json_printer.print(&start);
+            let ast_str = json_printer.print(&start);
+            return JSON::parse(&ast_str).unwrap_or(JsValue::UNDEFINED);
         }
     }
-    "{}".into()
+    JsValue::UNDEFINED
 }
--- tests/web.rs
+++ tests/web.rs
@@ -3,6 +3,7 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use js_sys::JSON;
 use tangibl_wasm::{parse, JsonTopCode};
 use wasm_bindgen::JsValue;
 use wasm_bindgen_test::*;
@@ -10,8 +11,8 @@ use wasm_bindgen_test::*;
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn parses_a_start_token() {
-    let mut topcodes = serde_wasm_bindgen::to_value(&vec![JsonTopCode {
+fn parses_a_start_token() -> Result<(), JsValue> {
+    let topcodes = serde_wasm_bindgen::to_value(&vec![JsonTopCode {
         code: 61,
         unit: 8.0,
         orientation: 0.0,
@@ -21,5 +22,6 @@ fn parses_a_start_token() {
     .unwrap();
     eprintln!("{:?}", topcodes);
     let result = parse(topcodes);
-    assert_eq!("{\"name\":\"start\"}", result);
+    assert_eq!(JSON::parse("{\"name\":\"start\"}")?, result);
+    Ok(())
 }
